3c5971328b07f371b78e00986f7cb81bf5eed410
Wellington Rogério <wrogerio.ing@gmail.com>
2022-11-04 13:10:04 -0300

    ajustes layout

--- pages/api/ativos/index.js
+++ pages/api/ativos/index.js
@@ -3,10 +3,13 @@ import { GetAtivos, PostChamado } from "../../../controllers/chamadosController"
 module.exports = async (req, res) => {
   switch (req.method) {
     case "GET":
-      // enable cors
+      // disable cors
       res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-      res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, Authorization, Content-Type, Accept");
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Content-Type", "application/json");
+
       const chamados = await GetAtivos();
       res.status(200).json(chamados);
       break;
